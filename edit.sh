abcc9bc6454fcc2159a5a8d0c06e6d7a47ece953
Sunkworld <365062829@qq.com>
2019-02-23 04:41:31 +0000

    modify edit.sh

--- edit.sh
+++ edit.sh
@@ -6,5 +6,5 @@ tar -xvf profiles.tar
 rm profiles.tar
 cd stylegan/
 python dataset_tool.py create_from_images datasets/custom-dataset ../profiles/
-fname=`ls /content/gdrive/My\ Drive/network*`
+fname=`ls network*`
 sed -i 's/= \"network-snapshot-.*\.pkl/= \"'$fname'/g' training/training_loop.py
